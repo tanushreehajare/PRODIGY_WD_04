635693334654a0b5e11771bb0923940ea047396f
Tanushree Hajare <137613783+tanushreehajare@users.noreply.github.com>
2023-09-26 20:52:45 +0530

    Update about.js

--- portfolio/about.js
+++ portfolio/about.js
@@ -6,28 +6,26 @@ var typedAbtLogo = new Typed(".abtlogo", {
     loop: false,
     showCursor: false
 });
-// Select all elements with the class "animate-element"
+
 const animatedElements = document.querySelectorAll('.animate-element');
 
-// Callback function when the elements intersect with the viewport
 const animateCallback = (entries, observer) => {
   entries.forEach((entry) => {
     if (entry.isIntersecting) {
-      // Add the "active" class to start the animation
+ 
       entry.target.classList.add('active');
-      // Stop observing the element to prevent the animation from occurring multiple times
+
       observer.unobserve(entry.target);
     }
   });
 };
 
-// Create an Intersection Observer
+
 const observer = new IntersectionObserver(animateCallback, {
-  root: null, // Use the viewport as the root
-  threshold: 0.2, // Trigger the animation when 20% of the element is visible
+  root: null, 
+  threshold: 0.2, 
 });
 
-// Observe each animated element
 animatedElements.forEach((element) => {
   observer.observe(element);
 });
@@ -36,8 +34,8 @@ const progressBars = document.querySelectorAll('.progress-line span');
 const animateProgressBar = (entries, observer) => {
   entries.forEach((entry) => {
     if (entry.isIntersecting) {
-      entry.target.style.width = entry.target.dataset.width; // Set the width
-      entry.target.style.transform = 'translateX(0)'; // Slide in from the left
+      entry.target.style.width = entry.target.dataset.width;
+      entry.target.style.transform = 'translateX(0)'; 
       observer.unobserve(entry.target);
     }
   });
@@ -49,12 +47,12 @@ const progressBarObserver = new IntersectionObserver(animateProgressBar, {
 });
 
 progressBars.forEach((bar) => {
-  bar.dataset.width = bar.style.width; // Store the width value in a data attribute
-  bar.style.width = '0'; // Initialize width to zero
+  bar.dataset.width = bar.style.width;
+  bar.style.width = '0';
   progressBarObserver.observe(bar);
 });
 function scrollToTop() {
-  // Scroll to the top of the page smoothly
+
   window.scrollTo({
       top: 0,
       behavior: 'smooth'
